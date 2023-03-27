dda56f31570821187dfd6ac1af24e221773aaa68
PCserver0 <pcserver@rambler.ru>
2023-03-27 18:21:10 +0300

    HotFix update_activity

--- mobile_scraper/polling/update_activity.py
+++ mobile_scraper/polling/update_activity.py
@@ -3,10 +3,33 @@ import datetime
 
 from main import BASE_DIR
 from mobile_scraper.GoogleSheets.gsheets import write_column
-from mobile_scraper.updater import get_local_links, get_product_links_from_page
+from mobile_scraper.updater import get_local_links, HOST
+from mobile_scraper.scraper_main import get_htmlsoup
 
 
 # по сути переопределение функции из updater для другого текстового файла во избежание конфликтов
+def get_product_links_from_page(url):
+    soup = get_htmlsoup(url)
+
+    # поиск и запись в массив всех ссылок на товары со страницы
+    soup_a_li = soup.find_all('a', class_='vehicle-data track-event u-block js-track-event js-track-dealer-ratings')
+    with open(os.path.join(BASE_DIR, "mobile_scraper", "data", "activity_links.txt"), "a") as al_output:
+        for link in soup_a_li:
+            try:
+                al_output.write(HOST + link.get('href') + "\n")
+            except Exception as excep:
+                print('error: links not found', excep)
+    print(len(soup_a_li), url)
+
+    # переход на следующую страницу
+    if soup.find('a', class_='pagination-nav pagination-nav-right btn btn--muted btn--s'):
+        try:
+            get_product_links_from_page(HOST + soup.find('a', class_='pagination-nav pagination-nav-right btn '
+                                                                     'btn--muted btn--s').get('href'))
+        except Exception as excep:
+            print('warning: next page link not found', excep)
+
+
 def get_all_active_links():
     # очистка содержимого active_links.txt
     with open(os.path.join(BASE_DIR, "mobile_scraper", "data", "activity_links.txt"), "w"):
@@ -35,7 +58,6 @@ def update_products_activity():
     # вычитаем множества и получаем ссылки, которые надо пометить неактивными в таблице +
     # необходимо спарсить и дозагрузить в таблице
     del_links = local_set - active_set
-    upd_links = active_set - local_set
 
     # отмечаем неактивные товары в таблице
     activity_row = []
