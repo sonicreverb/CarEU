0a842ed907ba645911953f08ceffb88b088c54c1
PCserver0 <pcserver@rambler.ru>
2023-07-12 02:23:34 +0300

    STABLE LINEAR VERSION WITH 1 SEC SLEEP AT EVERY REQUEST TO SITE (added whle True in main.py)

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import datetime
 import time
 
-from mobile_scraper.database.database_main import upload_db_data_to_xlsx, update_tcalc, update_final_prices
+from mobile_scraper.database.database_main import upload_db_data_to_xlsx, update_tcalc,  update_final_prices
 from mobile_scraper.parser import start_parser, start_activity_update,  upload_db_to_ftp
 
 
 if __name__ == "__main__":
     while True:
         begin_time = time.time()
+
         start_activity_update()
         start_parser()
+
         last_request_time = time.time()
+        update_tcalc()
         update_final_prices()
         upload_db_data_to_xlsx()
         upload_db_to_ftp()
