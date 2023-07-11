534dc1f04d54e0f1ba548c67622fe71d9cf647ca
PCserver0 <pcserver@rambler.ru>
2023-07-12 02:22:12 +0300

    STABLE LINEAR VERSION WITH 1 SEC SLEEP AT EVERY REQUEST TO SITE (added whle True in main.py)

--- main.py
+++ main.py
@@ -6,15 +6,16 @@ from mobile_scraper.parser import start_parser, start_activity_update,  upload_d
 
 
 if __name__ == "__main__":
-    begin_time = time.time()
-    start_activity_update()
-    start_parser()
-    last_request_time = time.time()
-    update_final_prices()
-    upload_db_data_to_xlsx()
-    upload_db_to_ftp()
+    while True:
+        begin_time = time.time()
+        start_activity_update()
+        start_parser()
+        last_request_time = time.time()
+        update_final_prices()
+        upload_db_data_to_xlsx()
+        upload_db_to_ftp()
 
-    if time.time() - last_request_time < 3600:
-        print(f"[MAIN PROCESS] ({datetime.datetime.now()}) Сессия завершена за {time.time() - begin_time}."
-              f" Время ожидания {time.time() - last_request_time} ")
-        time.sleep(time.time() - last_request_time)
+        if time.time() - last_request_time < 3600:
+            print(f"[MAIN PROCESS] ({datetime.datetime.now()}) Сессия завершена за {time.time() - begin_time}."
+                  f" Время ожидания {time.time() - last_request_time} ")
+            time.sleep(time.time() - last_request_time)
